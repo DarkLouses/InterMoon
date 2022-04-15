11aae03a98375106510f1c6aea7e5f3ecbfb204f
gabriel <gabrielenriquevargas2001@gmail.com>
2022-04-15 20:29:31 +0200

    progreso

---
+++ Formulario/Formulario.php
@@ -0,0 +1,17 @@
+<?php
+
+$nombre = $_POST['nombre'];
+$apellido = $_POST['apellido'];
+$usuario = $_POST['usuario'];
+$email = $_POST['email'];
+$contraseña = $_POST['contraseña'];
+$telefono = $_POST['telefono'];
+
+
+echo $nombre.  "<br>";
+echo $apellido. "<br>";
+echo $usuario.  "<br>";
+echo $email.  "<br>";
+echo $contraseña. "<br>";
+echo $telefono;
+?>
--- Scripts.js
+++ Scripts.js
@@ -1,46 +1,57 @@
 document.addEventListener('DOMContentLoaded' , function(event) {
 
+    document.getElementById("php").addEventListener("click" , php);
+    document.getElementById("sql").addEventListener("click" , sql);
+    document.getElementById("javascript").addEventListener("click" , javascript);
+    document.getElementById("java").addEventListener("click" , java);
+
 
     carrusel();
-    document.getElementById("reg").addEventListener("click" , nombre);
-
 });
 
-// Al hacer click en el botón Registrarse, aparecerá un 'prompt' con el mensaje: 'Introduce tu nombre:"
-// Después, el texto introducido por el usuario se mostrará en un div a la izquierda de la página (arriba) como mensaje:
-// 'Bienvenido [texto/nombre]'.
-
-function nombre() {
-
-   var palabra = prompt("tu nombre");
-   var mensaje = "ongi etorri , " +palabra;
-   document.getElementById("bienvenido").innerHTML = mensaje;
-
-
-}
-
-// Esta función tomará aleatoriamente la dirección de una imagen del carrusel
-// y se la añadirá al elemento 'imagen'
 function imagenAleatoria(argument) {
+
     var imagenes = [
-        'img/carrusel/1.png',
-        'img/carrusel/2.png',
-        'img/carrusel/3.png'
+        'imagenes/carrusel/img1.jpg',
+        'imagenes/carrusel/img2.png',
+        'imagenes/carrusel/img3.png',
+        'imagenes/carrusel/img4.jpg',
+        'imagenes/carrusel/img5.jpg'
     ];
     var index = Math.floor((Math.random() * imagenes.length));
-
     var imagen_actual = imagenes[index];
-    //TODO: insertar la imagen actual en el elemento 'imagen'
 
    document.getElementById("carru").src = imagen_actual;
 
 }
 
+function carrusel() {
 
-// llamará a la función imagenAleatoria y esperará 3000 milisegundos
-// hasta volver a llamar a la misma función
-function carrusel(){
     imagenAleatoria();
     setInterval(imagenAleatoria, 3000);
  
+}
+
+function php() {
+ 
+    location.href = "./Formulario/iniciarSeccion.html"
+
+ }
+
+function sql() {
+ 
+    location.href = "./Formulario/iniciarSeccion.html"
+   
+}
+
+function javascript() {
+ 
+    location.href = "./Formulario/iniciarSeccion.html"
+       
+}
+
+function java() {
+ 
+    location.href = "./Formulario/iniciarSeccion.html"
+           
 }
